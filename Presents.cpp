d31be6569539da644966edea975799445193f90e
C243158 <sahilmujtaba05@gmail.com>
2025-06-17 23:10:27 +0600

    Update Presents.cpp

--- Presents.cpp
+++ Presents.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+int n;
+cin>>n;
+vector<int> v(n);
+for (int i = 0; i<n; i++){
+    int p;
+    cin>>p;
+    v[p-1] = i+1;
+}
+cout<<endl;
+for(auto i : v){
+    cout<<i<< " ";
+}
+    return 0;
+}
